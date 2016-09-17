a37a2c3ae56b5a738d30da79d67d48f17e2440a0
Alexandr Kursov <kursov1986@gmail.com>
2016-09-18 00:30:31 +0300

    Created MainActivity class and layout

---
+++ app/src/main/java/com/kursov/remindme/MainActiity.java
@@ -0,0 +1,13 @@
+package com.kursov.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActiity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actiity_main);
+    }
+}
